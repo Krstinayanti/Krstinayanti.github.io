379bf56c1b91fb605206a260594b0b31e3c43b7f
Kristina Yanti <kristinayanti@MacBook-Air-Kristina.local>
2020-03-21 22:58:18 +0800

    first launch

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kristina Yanti</title>
+
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="assets/css/bulma.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+    
+    <div class="background"></div>
+
+    <div class="wrapper">
+        <div class="container">
+            <section class="hero">
+                <div class="hero-body">
+                    <div class="container">
+                    <h1 class="title">
+                        Halo!
+                    </h1>
+                    <h2 class="subtitle">
+                        Namaku <strong>Kristina Yanti</strong>
+                        <br> Salam kenal!:) 
+                    </h2>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section referer">
+                <ul>
+                    <li><i class="fab fa-internet-explorer"></i>&nbsp;&nbsp;<a href="bloggerkristinayanti36.blogspot.com" target="_blank" class="link">Blogger</a></li>
+                    <li><i class="fab fa-github"></i>&nbsp;&nbsp;<a href="https://github.com/Krstinayanti" target="_blank" class="link">GitHub</a></li>
+                    <li><i class="fab fa-facebook"></i>&nbsp;&nbsp;<a href="https://www.facebook.com/kristina.yanti.716" target="_blank" class="link">Facebook</a></li>
+                    <li><i class="fab fa-instagram"></i>&nbsp;&nbsp;<a href="https://www.instagram.com/krstinayanti/" target="_blank" class="link">Instagram</a></li>
+                </ul>
+            </section>
+        </div>
+    </div>
+
+    <script src="assets/js/all.min.js"></script>
+
+</body>
+</html>
